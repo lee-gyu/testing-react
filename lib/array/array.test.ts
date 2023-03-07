2e5cb28f839ed85cb78afcc76afa605abf964e7f
LeeGyu <rbcjf0219@gmail.com>
2023-03-08 00:09:00 +0900

    unit test 추가

--- lib/array/array.test.ts
+++ lib/array/array.test.ts
@@ -1,7 +1,25 @@
 import { describe, it, expect } from 'vitest';
+import { ArrayAPI, PRIMITIVE_COMPARATOR } from '.';
 
 describe('lib/array', () => {
-  it('max', () -> {
-    
+  it('max with number[]', () => {
+    const list = [1, 2, 3, 4];
+    const { max } = ArrayAPI(PRIMITIVE_COMPARATOR);
+
+    expect(max(...list)).toBe(4);
+  });
+
+  it('max with number[]', () => {
+    const list = [1, 2, 3, 4];
+    const { max } = ArrayAPI(PRIMITIVE_COMPARATOR);
+
+    expect(max(...list)).toBe(4);
+  });
+
+  it('max with empty list', () => {
+    const list: number[] = [];
+    const { max } = ArrayAPI(PRIMITIVE_COMPARATOR);
+
+    expect(() => max(...list)).toThrowError();
   });
 });
--- lib/array/index.ts
+++ lib/array/index.ts
@@ -1,1 +1,12 @@
-export const max = () => {};
+export const PRIMITIVE_COMPARATOR = (a: number, b: number) => (a < b ? -1 : 1);
+
+export const ArrayAPI = <T>(comparator: (a: T, b: T) => number) => {
+  return {
+    max(...items: T[]) {
+      if (items.length === 0)
+        throw new Error('Could not get max() by empty list.');
+
+      return items.reduce((a, b) => (comparator(a, b) < 0 ? b : a), items[0]);
+    },
+  };
+};
